bf75c3344e8661b482f674785db399f5f9b6561c
Eduardo Carneiro da Silva <eduardo.carneiro@t2mlab.com>
2024-03-28 09:40:01 -0300

    Primeiro commit

---
+++ ApiCatalogo/ApiCatalogo/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ApiCatalogo.Models
+{
+    public class Categoria
+    {
+
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>(); //Aqui estou inicializando a propriedade Produto que é uma coleção de objeto produto(éuma boa pratica)
+        }
+
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
+        public ICollection<Produto>? Produtos { get; set; } // Aqui estou definindo Que uma Categoria pode ter muitos Produtos
+
+    }
+}
---
+++ ApiCatalogo/ApiCatalogo/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiCatalogo.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }   
+        public decimal Preco {  get; set; } 
+        public string? ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public int CategoriaId { get; set; } // Aqui vai mapear para coluna CategoriaID que vai ser criada na tabela Produto
+        public Categoria? Categoria { get; set; } // Aqui é uma propriedade de navegação que define que produto ta mapeado para uma categoria
+    }
+}
